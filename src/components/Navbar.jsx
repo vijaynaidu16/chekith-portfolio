15e7762ba7ea3354ad774dd2754256c224cd664e
vijay <digitalxvijay@gmail.com>
2024-11-12 16:11:45 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <h2>Chekith </h2>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
