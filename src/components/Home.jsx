ca2236df364c0db6eda44c589c0a18306b12cbb9
vijay <digitalxvijay@gmail.com>
2024-11-12 16:14:43 +0530

    Fixed: Name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import { LOGO } from "../utils/constants"
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Home = () => {
-    const [login, setLogin] = useState(true);
-
-    const handle = () => {
-        setLogin(!login);
-    }
-
   return (
     <div className="home">
         <div className="home-1">
-           <h1> Hi, I am Chekith & </h1>
+           <h1> Hi, I am Chekith Shankarpally & </h1>
            <h2>I am a UI UX Designer</h2>
            <div className="icons">
            <a href="https://github.com/chekith" target="_blank"><spam><FaGithub /></spam></a>
